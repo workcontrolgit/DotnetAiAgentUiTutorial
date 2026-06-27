5d504e66116b1502b2f1b6807e6e7475f8ec965a
Fuji Nguyen <fuji.nguyen@workcontrol.com>
2026-06-26 23:50:58 -0400

    feat(migrations): add AddIdentity migration for ASP.NET Core Identity tables

--- DotnetAiAgentUi/src/HrMcp.Infrastructure.Persistence/Migrations/20260627035043_AddIdentity.Designer.cs
+++ DotnetAiAgentUi/src/HrMcp.Infrastructure.Persistence/Migrations/20260627035043_AddIdentity.Designer.cs
@@ -4,6 +4,7 @@ using HrMcp.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HrMcp.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(HrDbContext))]
-    partial class HrDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260627035043_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
